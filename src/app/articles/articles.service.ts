22dd788613d794395e0c2ef966cd4463da892394
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-10-06 13:51:07 +0200

    Refactor ArticlesService to use Observable

--- src/app/articles/articles.service.ts
+++ src/app/articles/articles.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { Article } from './article';
 import { ARTICLES } from './mock-articles';
 
@@ -10,7 +13,13 @@ export class ArticlesService {
 
   constructor() { }
 
-  getArticles(): Article[] {
-    return ARTICLES;
+  getArticles(): Observable<Article[]> {
+    return of(ARTICLES);
+  }
+
+  getArticle(articleId: number) {
+    return this.getArticles().pipe(
+      map((articles: Article[]) => articles.find(article => article.id === articleId)!)
+    );
   }
 }
