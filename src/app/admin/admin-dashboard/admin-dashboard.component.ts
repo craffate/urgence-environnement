42435317e05c757f5d37fba8915c250e45735c61
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-10-07 11:16:03 +0200

    Add expandable data rows

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 import { Article } from '../../articles/article';
 import { ArticlesService } from '../../articles/articles.service';
@@ -9,11 +10,22 @@ import { ArticlesService } from '../../articles/articles.service';
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.css']
+  styleUrls: ['./admin-dashboard.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class AdminDashboardComponent implements OnInit {
 
   articles$!: Observable<Article[]>;
+
+  columnsToDisplay: string[] = ['name', 'subtitle', 'description', 'price']
+
+  expandedArticle: Article | null = null;
 
   constructor(
     private articlesService: ArticlesService
