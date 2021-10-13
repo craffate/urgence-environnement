462b7e256cea3f56a14333244fc4b57dae3f1b2a
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-10-13 12:49:41 +0200

    Remove mock-articles.ts

--- src/app/articles/mock-articles.ts
+++
@@ -1,7 +0,0 @@
-import { Article } from './article';
-
-export const ARTICLES: Article[] = [
-  { id: 1, name: 'Table en bois', subtitle: 'Classique', description: 'Table en bois', price: 20 },
-  { id: 2, name: 'Table en plastique', subtitle: 'Economique', description: 'Table en plastique', price: 5 },
-  { id: 3, name: 'Table en marbre', subtitle: 'Sublime', description: 'Table en marbre', price: 50 }
-];
