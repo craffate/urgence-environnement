0d8f5583286a473e6046f2323a2ba688e234a1a2
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-10-19 21:51:58 +0200

    Edit AdminModule, ArticleImageComponent

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { FormsModule } from '@angular/forms';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-
+import { MatIconModule } from '@angular/material/icon';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { ArticleEditorComponent } from './article-editor/article-editor.component';
@@ -28,11 +29,13 @@ import { ArticleImageComponent } from './article-image/article-image.component';
   imports: [
     CommonModule,
     FormsModule,
+    FlexLayoutModule,
     MatTableModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatDialogModule,
+    MatIconModule,
     AdminRoutingModule
   ],
   entryComponents: [
--- src/app/admin/article-image/article-image.component.ts
+++ src/app/admin/article-image/article-image.component.ts
@@ -1,34 +1,53 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, AfterViewInit } from '@angular/core';
 
 import { Article } from '../../articles/article';
 
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Observable, of } from 'rxjs';
+
+import { ArticlesService } from 'src/app/articles/articles.service';
 
 @Component({
   selector: 'app-article-image',
   templateUrl: './article-image.component.html',
   styleUrls: ['./article-image.component.css']
 })
-export class ArticleImageComponent implements OnInit {
+export class ArticleImageComponent implements OnInit, AfterViewInit {
 
-  image!: File;
+  slideIndex: number = 0;
+  images!: string[];
   fd: FormData = new FormData();
 
   constructor(
     public dialogRef: MatDialogRef<ArticleImageComponent>,
-    @Inject(MAT_DIALOG_DATA) public article: Article
+    @Inject(MAT_DIALOG_DATA) public article: Article,
+    private articlesService: ArticlesService
   ) { }
 
   ngOnInit(): void {
   }
 
-  fileChosen(event: any) {
-    this.image = event.target.files[0];
+  ngAfterViewInit(): void {
+    this.articlesService.getImages(this.article.id).subscribe((res) => this.images = res);
   }
 
-  addImage() {
-    this.fd.append("article", this.image);
+  fileChosen(event: any): void {
+    this.fd.set("article", event.target.files[0]);
+  }
+
+  addImage(): void {
     this.dialogRef.close(this.fd);
+  }
+
+  changeSlide(n: number): void {
+    this.slideIndex += n;
+
+    if (this.slideIndex >= this.images.length) {
+      this.slideIndex = 0;
+    }
+    if (this.slideIndex < 0) {
+      this.slideIndex = this.images.length - 1;
+    }
   }
 
 }
