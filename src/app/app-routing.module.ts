63c3e4b5715618f20e9044d352bdbaabecfe9375
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-11-01 17:06:19 +0100

    Add simple admin page guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminGuard } from './guards/admin.guard';
 
 const routes: Routes = [
   {
@@ -8,7 +9,8 @@ const routes: Routes = [
   },
   {
   path: 'admin',
-  loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+  canActivate: [AdminGuard]
   },
   {
     path: 'auth',
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
 import { ApiPaths } from '@src/api-paths';
 
 import { User } from '@interfaces/user';
+import { UserService } from '@services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private httpClient: HttpClient) { }
+  authenticated: boolean = false;
+  admin: boolean = false;
+
+  constructor(
+    private httpClient: HttpClient,
+    public userService: UserService,
+  ) {
+    userService.getProfile().subscribe((res) => {
+      this.authenticated = !!res;
+      this.admin = !!(res.role === 1);
+    });
+  }
 
   signIn(user: User) {
     return this.httpClient.post(`${environment.apiUrl}${ApiPaths.Auth}/signin`, user, { responseType: 'text', withCredentials: true });
