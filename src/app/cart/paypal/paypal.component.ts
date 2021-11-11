f71d10002a3db65e286957dd4b13b621dabf8338
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-11-11 15:11:00 +0100

    Edit PayPalComponent

--- src/app/cart/paypal/paypal.component.ts
+++ src/app/cart/paypal/paypal.component.ts
@@ -1,4 +1,7 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { CartService } from '@services/cart.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 declare var paypal: any;
 
@@ -9,11 +12,70 @@ declare var paypal: any;
 })
 export class PaypalComponent implements OnInit {
   @ViewChild('paypal', { static: true }) paypalElement!: ElementRef;
+  @Output() transactionStatus: EventEmitter<number> = new EventEmitter<number>();
 
-  constructor() { }
+  purchase_items$!: Observable<any[]>;
+  total$!: Observable<number>;
+
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit(): void {
-    paypal.Buttons().render(this.paypalElement.nativeElement);
+    this.purchase_items$ = this.cartService.getArticles().pipe(
+      map((articles) => {
+        return articles.map((article) => {
+          return {
+            sku: article.sku,
+            name: article.name,
+            unit_amount: {
+              currency_code: 'EUR',
+              value: article.price.toString()
+            },
+            currency: 'EUR',
+            quantity: '1',
+            category: 'PHYSICAL_GOODS'
+          }
+        })
+      })
+    );
+    this.total$ = this.cartService.calculateTotal();
+    paypal.Buttons({
+      createOrder: (data: any, actions: any) => {
+        let purchase_items;
+        let total;
+
+        this.purchase_items$.subscribe((res) => purchase_items = res);
+        this.total$.subscribe((res) => total = res.toString());
+
+        return actions.order.create({
+          purchase_units: [
+            {
+              amount: {
+                currency_code: 'EUR',
+                value: total,
+                breakdown: {
+                  item_total: {
+                    currency_code: 'EUR',
+                    value: total
+                  }
+                }
+              },
+              items: purchase_items
+            },
+          ]
+        });
+      },
+      onApprove: (data: any, actions: any) => {
+        this.transactionStatus.emit(0);
+      },
+      onCancel: (data: any, actions: any) => {
+        this.transactionStatus.emit(1);
+      },
+      onError: (error: any) => {
+        this.transactionStatus.emit(2);
+      }
+    }).render(this.paypalElement.nativeElement);
   }
 
 }
