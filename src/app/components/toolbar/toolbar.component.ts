42fcf92d52348ae797dc65442448897f40dce374
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-11-16 11:11:01 +0100

    Add CategoryService

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '@src/app/services/auth.service';
 import { CartService } from '@src/app/services/cart.service';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Category } from '@interfaces/category';
+import { CategoryService } from '@src/app/services/category.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -12,31 +13,20 @@ import { Category } from '@interfaces/category';
 export class ToolbarComponent implements OnInit {
 
   @Input() title!: string;
-  readonly categories: Category[];
+  categories$: Observable<Category[]>;
   activeLink!: string;
   isAuth$: Observable<boolean>;
 
   constructor(
+    private categoryService: CategoryService,
     private cartService: CartService,
     private authService: AuthService
   ) {
-    this.categories = [
-      { id: 1, name: 'Mobilier', slug: 'mobilier' },
-      { id: 2, name: 'Librairie', slug: 'librairie' },
-      { id: 3, name: 'Loisir', slug: 'loisir' },
-      { id: 4, name: 'Bricolage', slug: 'bricolage' },
-      { id: 5, name: 'Brocante', slug: 'brocante' },
-      { id: 6, name: 'Objets d\'occasion', slug: 'occasion' },
-      { id: 7, name: 'Divers', slug: 'divers' },
-    ];
+    this.categories$ = this.categoryService.getCategories();
     this.isAuth$ = this.authService.isAuth$;
   }
 
   ngOnInit(): void {
-  }
-
-  getCartTotal(): BehaviorSubject<number> {
-    return this.cartService.cartTotal$;
   }
 
   getCartQuantity(): BehaviorSubject<number> {
---
+++ src/app/services/category.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '@interfaces/category';
+
+import { environment } from '@environments/environment';
+import { ApiPaths } from '@src/api-paths';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getCategories(params?: HttpParams): Observable<Category[]> {
+    return this.httpClient.get<Category[]>(`${environment.apiUrl}${ApiPaths.Categories}`, { params: params });
+  }
+
+  postCategory(category: Category): Observable<Category> {
+    return this.httpClient.post<Category>(`${environment.apiUrl}${ApiPaths.Articles}`, category);
+  }
+
+  getArticle(categoryId: number): Observable<Category> {
+    return this.httpClient.get<Category>(`${environment.apiUrl}${ApiPaths.Articles}/${categoryId}`);
+  }
+
+  patchArticle(categoryId: number, category: Category): Observable<Category> {
+    return this.httpClient.patch<Category>(`${environment.apiUrl}${ApiPaths.Articles}/${categoryId}`, category);
+  }
+
+  deleteArticle(categoryId: number): Observable<any> {
+    return this.httpClient.delete(`${environment.apiUrl}${ApiPaths.Articles}/${categoryId}`);
+  }
+
+}
