a79cda5547ec3d3a7b5a37c2798447465ca916ff
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-11-16 11:36:02 +0100

    Edit ArticleAddComponent

--- src/app/admin/article-add/article-add.component.ts
+++ src/app/admin/article-add/article-add.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { FormGroup, FormControl } from '@angular/forms';
 
 import { MatDialogRef } from '@angular/material/dialog';
+import { Category } from '@src/app/interfaces/category';
+import { CategoryService } from '@src/app/services/category.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-article-add',
@@ -12,16 +15,21 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class ArticleAddComponent implements OnInit {
 
   formGroup: FormGroup;
+  categories$: Observable<Category[]>;
 
   constructor(
     public dialogRef: MatDialogRef<ArticleAddComponent>,
+    private categoryService: CategoryService
   ) {
+    this.categories$ = this.categoryService.getCategories();
     this.formGroup = new FormGroup({
       sku: new FormControl(''),
       name: new FormControl(''),
       subtitle: new FormControl(''),
       description: new FormControl(''),
-      price: new FormControl(0.0)
+      quantity: new FormControl(1),
+      price: new FormControl(0.0),
+      Category: new FormControl(undefined)
     });
   }
 
