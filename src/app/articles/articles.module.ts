a105b1858977f6f3a0cf28421439c8bd939708ae
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-11-22 12:30:02 +0100

    Move SearchComponent to SharedModule

--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -1,45 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ArticlesRoutingModule } from './articles-routing.module';
 import { ArticlesListComponent } from './articles-list/articles-list.component';
 import { ArticleDetailComponent } from './article-detail/article-detail.component';
+import { ArticleCardComponent } from './article-card/article-card.component';
 
 import { SharedModule } from '../shared/shared.module';
-import { ArticleCardComponent } from './article-card/article-card.component';
-import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     ArticlesListComponent,
     ArticleDetailComponent,
     ArticleCardComponent,
-    SearchComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
     FlexLayoutModule,
-    ReactiveFormsModule,
     MatCardModule,
     MatGridListModule,
     MatButtonModule,
     MatIconModule,
     MatSnackBarModule,
     MatDividerModule,
-    MatInputModule,
-    MatFormFieldModule,
     ArticlesRoutingModule
   ]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { CarouselComponent } from './carousel/carousel.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
-    CarouselComponent
+    CarouselComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule,
     FlexLayoutModule,
+    ReactiveFormsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   exports: [
-    CarouselComponent
+    CarouselComponent,
+    SearchComponent
   ]
 })
 export class SharedModule { }
