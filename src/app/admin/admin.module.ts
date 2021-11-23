00e4fe52b2805f6d668f50bf67a5ac16fb696486
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-11-23 17:43:18 +0100

    Add ArticleFormComponent

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,35 +1,50 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 
 import { SharedModule } from '../shared/shared.module';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { ArticlesTableComponent } from './articles-table/articles-table.component';
+import { ArticleFormComponent } from './article-form/article-form.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
     ArticlesComponent,
-    ArticlesTableComponent
+    ArticlesTableComponent,
+    ArticleFormComponent
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
     SharedModule,
+    ReactiveFormsModule,
     FlexLayoutModule,
     MatCardModule,
     MatTabsModule,
     MatTableModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class AdminModule { }
---
+++ src/app/admin/article-form/article-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Article } from '@src/app/interfaces/article';
+
+@Component({
+  selector: 'app-article-form',
+  templateUrl: './article-form.component.html',
+  styleUrls: ['./article-form.component.css']
+})
+export class ArticleFormComponent implements OnInit {
+
+  @Input() article!: Article;
+
+  articleForm!: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.articleForm = new FormGroup({
+      id: new FormControl({ value: this.article.id, disabled: true }, Validators.required),
+      sku: new FormControl({ value: this.article.sku, disabled: true }),
+      name: new FormControl(this.article.name, [Validators.required, Validators.minLength(3)]),
+      subtitle: new FormControl(this.article.subtitle),
+      price: new FormControl(this.article.price, [Validators.required, Validators.min(0.01)]),
+      quantity: new FormControl(this.article.quantity, [Validators.required, Validators.min(0)]),
+      weight: new FormControl(this.article.weight, Validators.min(0.01)),
+      weight_unit: new FormControl(this.article.weight_unit),
+      length: new FormControl(this.article.length, Validators.min(0.01)),
+      width: new FormControl(this.article.width, Validators.min(0.01)),
+      height: new FormControl(this.article.height, Validators.min(0.01)),
+      dimensions_unit: new FormControl(this.article.dimensions_unit),
+    });
+  }
+
+  onSubmit(): void {
+  }
+
+}
