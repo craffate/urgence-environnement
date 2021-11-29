8b4bfe314f799cd505314dd0d0ed1bd808313291
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2021-11-29 15:34:08 +0100

    Edit ArticleFormComponent to handle categories

--- src/app/admin/article-form/article-form.component.ts
+++ src/app/admin/article-form/article-form.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Article } from '@src/app/interfaces/article';
-import { ArticlesService } from '@src/app/services/articles.service';
+import { Article } from '@interfaces/article';
+import { Category } from '@interfaces/category';
+import { ArticlesService } from '@services/articles.service';
+import { CategoryService } from '@services/category.service';
 
 @Component({
   selector: 'app-article-form',
@@ -13,15 +15,19 @@ export class ArticleFormComponent implements OnInit {
 
   @Input() article!: Article;
 
+  categories: Category[] = [];
+
   articleForm!: FormGroup;
 
   constructor(
-    private articlesService: ArticlesService
+    private articlesService: ArticlesService,
+    private categoryService: CategoryService
   ) { }
 
   ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(res => this.categories = res);
     if (!this.article) {
-      this.article = <Article>{};
+      this.article = <Article>{ Category: this.categories[0] };
     }
     this.articleForm = this.getArticleForm();
   }
@@ -41,6 +47,7 @@ export class ArticleFormComponent implements OnInit {
       width: new FormControl(this.article.width, Validators.min(0.01)),
       height: new FormControl(this.article.height, Validators.min(0.01)),
       dimensions_unit: new FormControl(this.article.dimensions_unit),
+      Category: new FormControl(this.article.Category, Validators.required)
     });
   }
 
@@ -50,6 +57,10 @@ export class ArticleFormComponent implements OnInit {
 
   onDefault(): void {
     this.articleForm = this.getArticleForm();
+  }
+
+  compareCategory(o1: Category, o2: Category): boolean {
+    return (o1.id === o2.id);
   }
 
 }
